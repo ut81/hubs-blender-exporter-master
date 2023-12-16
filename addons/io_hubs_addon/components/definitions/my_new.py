3aa25f740e9bf9601cead9b1e97e632949d43065
ut <utsavagg37@gmail.com>
2023-12-16 19:27:41 +0530

    updated my_new

--- addons/io_hubs_addon/components/definitions/my_new.py
+++ addons/io_hubs_addon/components/definitions/my_new.py
@@ -1,16 +1,19 @@
 import bpy
 from bpy.types import Operator
-
-from ..models import link
-from ..gizmos import CustomModelGizmo, bone_matrix_world
 from bpy.props import FloatProperty, StringProperty
+
+
 from ..hubs_component import HubsComponent
 from ..types import Category, PanelType, NodeType
 from .networked import migrate_networked
+from ..gizmos import CustomModelGizmo, bone_matrix_world
+from ..models import link
 
+
+    
 class MyTestButtonOperator(Operator):
     bl_idname = "link.my_test_button"
-    bl_label = "My Test Button"
+    bl_label = "Performance Check:"
     bl_options = {'REGISTER', 'UNDO'}
     href: StringProperty(name="Link URL", description="Link URL",
                          default="https://test.org")
@@ -22,14 +25,32 @@ class MyTestButtonOperator(Operator):
         name="FileSize Warning", description="test warning", default=20.0)
 
     def execute(self, context):
-        # Access all properties from the property group
-        href = self.href
-        poly_count_warning = self.polyCountWarning
-        texture_size = self.TextureSize
-        file_size = self.FileSize
+        
+        self.report({'INFO'}, f"Button Clicked! Href: {self.href}, Polycount Warning: {self.polyCountWarning}, Texture Size: {self.TextureSize}, File Size: {self.FileSize}")
 
-        self.report({'INFO'}, f"Button Clicked! Href: {href}, Polycount Warning: {poly_count_warning}, Texture Size: {texture_size}, File Size: {file_size}")
+       
         return {'FINISHED'}
+    
+    def draw(self, context):
+        layout = self.layout
+
+        
+
+        layout.label(text=f"Link URL: {self.href}")
+        layout.label(text=f"Polycount Warning: {self.polyCountWarning}")
+        layout.label(text=f"Texture Size: {self.TextureSize}")
+        layout.label(text=f"File Size: {self.FileSize}")
+
+    
+    def invoke(self, context, event):
+
+        wm=context.window_manager
+        return wm.invoke_props_dialog(self,width=500)
+    
+
+
+
+
 class Link(HubsComponent):
     _definition = {
         'name': 'my-new',
@@ -60,7 +81,7 @@ class Link(HubsComponent):
         row.prop(data=self, property="FileSize")
         
         row = layout.row()
-        op = row.operator("link.my_test_button", text="Click Me!")
+        op = row.operator("link.my_test_button", text="Export")
         op.href = self.href
         op.polyCountWarning = self.polyCountWarning
         op.TextureSize = self.TextureSize
@@ -70,11 +91,12 @@ class Link(HubsComponent):
     @classmethod
     def register(cls):
         bpy.utils.register_class(MyTestButtonOperator)
-
+        
     @classmethod
     def unregister(cls):
         bpy.utils.unregister_class(MyTestButtonOperator)
-    
+        
+        
         
 
     def migrate(self, migration_type, panel_type, instance_version, host, migration_report, ob=None):
